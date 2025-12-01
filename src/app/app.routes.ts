3339eb82a2a432cb47fda1ecc23cbf2728928405
farhad-khani@outlook.com <farhad-khani@outlook.com>
2025-12-02 01:08:21 +0330

    Login Form Made

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Loginpage } from './+pages/+public/loginpage/loginpage';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login',component:Loginpage},
+    {path:'',redirectTo:'login',pathMatch:'full'},
+    {path:'**',redirectTo:'login'}
+];
