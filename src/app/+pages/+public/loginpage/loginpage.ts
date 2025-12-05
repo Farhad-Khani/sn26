157b5795e1e95bc2348d8d9258708aaf5834e9e9
farhad-khani@outlook.com <farhad-khani@outlook.com>
2025-12-05 22:36:49 +0330

    End Of Model

--- src/app/+pages/+public/loginpage/loginpage.ts
+++ src/app/+pages/+public/loginpage/loginpage.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginModel } from './login.model';
 
 @Component({
   selector: 'app-loginpage',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './loginpage.html',
   styleUrl: './loginpage.css',
 })
 export class Loginpage {
-
+  login: LoginModel = {
+    username: '',
+    password: '',
+    remember: false,
+  };
+  message = '';
+  router = inject(Router);
+  check() {
+    if (this.login.username == 'admin' && this.login.password == 'admin') {
+      this.router.navigateByUrl('/admin');
+    }
+    else {
+      this.message = 'نام کاربری یا رمز ورود اشتباه است.';
+    }
+  }
 }
